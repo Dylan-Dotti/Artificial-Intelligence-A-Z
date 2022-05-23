ce3cf2d0a89e91b4cde837bf2849597e0b142b24
Dylan-Dotti <invocation42@yahoo.com>
2022-05-23 10:50:36 -0400

    Update ai.py

--- Self_Driving_Car/ai.py
+++ Self_Driving_Car/ai.py
@@ -2,3 +2,97 @@
 
 # Importing the libraries
 
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as tfunc
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+    
+    def forward(self, state):
+        x = tfunc.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+
+# Implementing experience replay
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+    
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+    
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+    
+# Implementing deep q learning
+
+class Dqn():
+    
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+    
+    def select_action(self, state):
+        probs = tfunc.softmax(self.model(Variable(state, volatile=True)) * 7) #t=7
+        action = probs.multinomial()
+        return action.data[0, 0]
+    
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = batch_reward + self.gamma * next_outputs
+        td_loss = tfunc.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_variables=True)
+        self.optimizer.step()
+    
+    def update(self, reward, new_signal):
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        self.memory.push((self.last_state, new_state, 
+                          torch.LongTensor([int(self.last_action)]),
+                          torch.Tensor([self.last_reward]))
+        self.last_state = new_state
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
